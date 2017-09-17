911e0a7e3e24fc2df062b319c7501e4c505bf9bb
Ishan <ishanatmuzaffarpur@gmail.com>
2017-09-17 12:41:35 +0530

    Fixed bug of side box position from top

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 var scrolled = false;
 var sideBox = $('#side-box');
 var mainBox = $('#main-box');
-var sideBoxTopOffset = sideBox.offset().top;
+
+var sideBoxTopPosition = sideBox.position().top;
 
 // Sets the variable to true everytime the window is scrolled
 $(window).scroll(function (event) {
@@ -16,10 +17,12 @@ setInterval(function () {
     'use strict';
     if (scrolled) {
         scrolled = false;
-        if ($(window).scrollTop() > sideBoxTopOffset) {
+        if ($(window).scrollTop() > sideBoxTopPosition) {
             sideBox.addClass('fixed-side-box');
         } else {
             sideBox.removeClass('fixed-side-box');
         }
     }
 }, 10);
+
+
