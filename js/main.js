7ed1673e214a1911cabc0dc164ed2a2b0be1c6ca
Ishan <ishanatmuzaffarpur@gmail.com>
2017-09-17 15:07:33 +0530

    Responsiveness fixed with javascript resize events

--- js/main.js
+++ js/main.js
@@ -1,44 +1,50 @@
-/*jslint browser: true*/
+/*jslint browser: true */
 /*global $, jQuery, console*/
 
-var scrolled = false;
+var BOOTSTRAP_LG_WIDTH = 992;
+
+var configChanged = true;
 var sideBox = $('#side-box');
 var mainBox = $('#main-box');
+var mainContainer = $(".main-container");
 
 var sideBoxTopPosition = sideBox.position().top;
 
-// Sets the variable to true everytime the window is scrolled
+// Sets the configChange flag everytime the window is scrolled
 $(window).scroll(function (event) {
     'use strict';
-    scrolled = true;
+    configChanged = true;
 });
-
-setInterval(function () {
+// Sets the configChange flag everytime the window is resized
+$(window).resize(function (event) {
     'use strict';
-    if (scrolled) {
-        scrolled = false;
-        if ($(window).scrollTop() > sideBoxTopPosition) {
-            sideBox.addClass('fixed-side-box');
-        } else {
-            sideBox.removeClass('fixed-side-box');
-        }
-    }
-}, 10);
+    configChanged = true;
+});
 
 var adjustSideBoxWidth = function () {
     'use strict';
-    var containerWidth = $(".main-container").width();
-    if (containerWidth >= 1336) {
+    if (mainContainer.width() >= 1336) {
         sideBox.addClass('fixed-side-box-width');
     } else {
         sideBox.removeClass('fixed-side-box-width');
     }
-    console.log(containerWidth + ' resize ' + containerWidth / 3);
 };
 
-adjustSideBoxWidth();
-
-$(window).resize(function (event) {
+var adjustSideBoxFixedTop = function () {
     'use strict';
-    adjustSideBoxWidth();
-});
+    if (($(window).scrollTop() > sideBoxTopPosition) && (window.innerWidth >= BOOTSTRAP_LG_WIDTH)) {
+        sideBox.addClass('fixed-side-box');
+    } else {
+        sideBox.removeClass('fixed-side-box');
+    }
+};
+
+// Check for configChanged and reset the UI
+setInterval(function () {
+    'use strict';
+    if (configChanged) {
+        configChanged = false;
+        adjustSideBoxFixedTop();
+        adjustSideBoxWidth();
+    }
+}, 10);
